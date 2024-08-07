85c70fd1b9741b484fc9acbe0e40cb358c0a7fe9
DecdKal <kalinmirchev9999@gmail.com>
2024-08-08 02:03:06 +0300

    adding seperate project for events rest
    events rest service

---
+++ blog-events/src/main/java/project/blog/events/BlogEventsApplication.java
@@ -0,0 +1,13 @@
+package project.blog.events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogEventsApplication.class, args);
+	}
+
+}
---
+++ blog-events/src/test/java/project/blog/events/BlogEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package project.blog.events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
